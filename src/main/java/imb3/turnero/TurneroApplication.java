b24190e7f27dba205d2bd89ca11e1ee98c4efc0f
Mario Ahumada <ahumadamob@gmail.com>
2023-06-11 16:47:19 -0300

    Primer commit (profesor)

---
+++ src/main/java/imb3/turnero/TurneroApplication.java
@@ -0,0 +1,13 @@
+package imb3.turnero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurneroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurneroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imb3/turnero/TurneroApplicationTests.java
@@ -0,0 +1,13 @@
+package imb3.turnero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurneroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
